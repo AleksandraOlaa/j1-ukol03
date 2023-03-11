11a75502493f9bc247f3e6cd6e1e51e540b76095
Aleksandra <ola.otlowska@gmail.com>
2023-03-11 17:53:08 +0100

    Procesor

---
+++ src/main/java/cz/czechitas/ukol3/Procesor.java
@@ -0,0 +1,32 @@
+package cz.czechitas.ukol3;
+
+public class Procesor {
+
+    private String vyrobce;
+
+    private long rychlost;
+
+    private String cpu;
+
+
+    public void setVyrobce(String vyrobce) {
+        this.vyrobce = vyrobce;
+    }
+
+    public void setRychlost(long rychlost) {
+        this.rychlost = rychlost;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public String getProcesor() {
+        toString();
+        {
+            return "Procesor" + " - " +
+                    "vyrobce procesoru je " + vyrobce + ", obsahuje " + cpu +
+                    ", a ma rychlost " + rychlost;
+        }
+    }
+}
