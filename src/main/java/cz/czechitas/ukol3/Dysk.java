c35ce578ad9c9e5cab0bf9620e325020652ddfca
Aleksandra <ola.otlowska@gmail.com>
2023-03-11 17:53:18 +0100

    Dysk

---
+++ src/main/java/cz/czechitas/ukol3/Dysk.java
@@ -0,0 +1,45 @@
+package cz.czechitas.ukol3;
+
+import java.lang.constant.Constable;
+
+public class Dysk {
+    private long kapacitaDysku;
+
+    private long vyuziteMisto;
+
+    private  String pevnyDysk;
+
+    public long velikost;
+
+    public void setKapacitaDysku (long kapacitaDysku){
+        this.kapacitaDysku = kapacitaDysku;
+    }
+
+    public void setVyuziteMisto (long vyuziteMisto){
+        this.vyuziteMisto = vyuziteMisto;
+    }
+
+    public void setPevnyDysk (String pevnyDysk){
+        this.pevnyDysk = pevnyDysk;
+    }
+
+    public String getDysk() {toString();
+        return "Dysk " + "je " + pevnyDysk +
+                ", ktery ma kapacitu " + kapacitaDysku +
+                ", z toho vyuzite misto je" + " " + vyuziteMisto;
+    }
+
+    public void setVelikost(long velikost) {
+        this.velikost = velikost;
+    }
+
+    /*
+    public void vytvorSouborOVelikosti(long velikost) {
+        long noveVyuziteMisto = velikost + vyuziteMisto;
+        return noveVyuziteMisto;
+    }
+     */
+
+    //public void vymazSouboryOVelikosti(long velikost);
+
+}
