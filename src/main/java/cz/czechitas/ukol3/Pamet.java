a783b163962b0ba1c461122e88a4c00f932b2b98
Aleksandra <ola.otlowska@gmail.com>
2023-03-11 17:52:40 +0100

    Pamet

---
+++ src/main/java/cz/czechitas/ukol3/Pamet.java
@@ -0,0 +1,26 @@
+package cz.czechitas.ukol3;
+
+public class Pamet {
+
+    private long kapacita;
+
+    private String ram;
+
+    public void setKapacita(long kapacita) {
+        this.kapacita = kapacita;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getPamet() {
+        toString();
+        {
+            return "Pamet " +
+                    "ma kapacitu " + kapacita + " a je to: " + ram;
+        }
+    }
+
+}
+
