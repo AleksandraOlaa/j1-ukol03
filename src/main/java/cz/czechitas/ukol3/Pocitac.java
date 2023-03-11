99fe17b92fb241798be7a8f0072c15ad032ae640
Aleksandra <ola.otlowska@gmail.com>
2023-03-11 17:53:31 +0100

    Pocitac

---
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -0,0 +1,39 @@
+package cz.czechitas.ukol3;
+
+public class Pocitac {
+
+    public boolean jeZapnuty;
+
+    public Pocitac() {
+        jeZapnuty = true;
+    }
+
+    public void isJeZapnuty() {
+        if (jeZapnuty = true) {
+            System.out.println("Pocitac je zapnuty");
+        } else {
+            System.err.println("Pocitac je vypnuty");
+        }
+    }
+
+    public void zapniSe() {
+        if (this.jeZapnuty = false) ;
+        {
+            this.jeZapnuty = true;
+            return;
+        }
+    }
+
+    public void vypniSe() {
+        if (this.jeZapnuty = true) ;
+        {
+            this.jeZapnuty = false;
+            return;
+        }
+    }
+
+
+
+}
+
+
