641b63766ff5aead0711a90dbac5370c80de82fd
Aleksandra <ola.otlowska@gmail.com>
2023-03-18 12:43:25 +0100

    Opraveny kod - Pocitac

--- src/main/java/cz/czechitas/ukol3/Pocitac.java
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -1,37 +1,54 @@
 package cz.czechitas.ukol3;
 
-public class Pocitac {
+import static java.lang.System.out;
 
+public class Pocitac {
     public boolean jeZapnuty;
 
-    public Pocitac() {
-        jeZapnuty = true;
+    private String ram;
+
+    private String cpu;
+
+    private String pevnyDysk;
+
+    public void setRam(String ram) {
+        this.ram = ram;
     }
 
-    public void isJeZapnuty() {
-        if (jeZapnuty = true) {
-            System.out.println("Pocitac je zapnuty");
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public void setPevnyDysk(String pevnyDysk) {
+        this.pevnyDysk = pevnyDysk;
+    }
+
+    public boolean jeZapnuty() {
+        return jeZapnuty;
+    }
+
+    public void vypniSe() {
+        if (this.ram == null || this.cpu == null || this.pevnyDysk == null) {
+            out.println("Pocitac neni zapnuty.");
         } else {
-            System.err.println("Pocitac je vypnuty");
+            if (jeZapnuty) {
+                System.err.println("Poictac je zapnuty. Muzes vypnout pocitac.");
+            }
         }
     }
 
     public void zapniSe() {
-        if (this.jeZapnuty = false) ;
-        {
-            this.jeZapnuty = true;
-            return;
+        if (jeZapnuty) {
+            out.println("Pocitac je zapnuty.");
+        } else {
+            if (this.ram == null || this.cpu == null || this.pevnyDysk == null) {
+                System.err.println("Pocitac je vypnuty.");
+            }
         }
     }
-
-    public void vypniSe() {
-        if (this.jeZapnuty = true) ;
-        {
-            this.jeZapnuty = false;
-            return;
-        }
-    }
-
 }
+
+
+
 
 
