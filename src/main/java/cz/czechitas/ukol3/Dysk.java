71e43bf320f14741e7f8d9de0d16c72b13ad00b7
Aleksandra <ola.otlowska@gmail.com>
2023-03-18 12:42:58 +0100

    Opraveny kod - Dysk

--- src/main/java/cz/czechitas/ukol3/Dysk.java
+++ src/main/java/cz/czechitas/ukol3/Dysk.java
@@ -1,29 +1,35 @@
 package cz.czechitas.ukol3;
 
-import java.lang.constant.Constable;
-
 public class Dysk {
     private long kapacitaDysku;
 
     private long vyuziteMisto;
 
-    private  String pevnyDysk;
+    private String pevnyDysk;
 
     public long velikost;
 
-    public void setKapacitaDysku (long kapacitaDysku){
+    public void setKapacitaDysku(long kapacitaDysku) {
         this.kapacitaDysku = kapacitaDysku;
     }
 
-    public void setVyuziteMisto (long vyuziteMisto){
+    public void setVyuziteMisto(long vyuziteMisto) {
         this.vyuziteMisto = vyuziteMisto;
     }
 
-    public void setPevnyDysk (String pevnyDysk){
+    public void getVyuziteMisto(long vyuziteMisto) {
+        this.vyuziteMisto = vyuziteMisto;
+    }
+
+    public void getKapacitaDysku(long kapacitaDysku) {
+        this.kapacitaDysku = kapacitaDysku;
+    }
+
+    public void setPevnyDysk(String pevnyDysk) {
         this.pevnyDysk = pevnyDysk;
     }
 
-    public String getDysk() {toString();
+    public String toString() {
         return "Dysk " + "je " + pevnyDysk +
                 ", ktery ma kapacitu " + kapacitaDysku +
                 ", z toho vyuzite misto je" + " " + vyuziteMisto;
@@ -33,13 +39,28 @@ public class Dysk {
         this.velikost = velikost;
     }
 
-    /*
-    public void vytvorSouborOVelikosti(long velikost) {
-        long noveVyuziteMisto = velikost + vyuziteMisto;
-        return noveVyuziteMisto;
-    }
-     */
 
-    // public void vymazSouboryOVelikosti(long velikost);
+    public long vytvorSouborOVelikosti(long velikost) {
+        if ((vyuziteMisto + velikost) < kapacitaDysku) {
+            setVyuziteMisto(velikost + vyuziteMisto);
+            System.out.println("Nove vyuzite misto je: " + vyuziteMisto);
+        } else {
+            System.err.println("Na soubor neni misto.");
+        }
+        return vyuziteMisto;
+    }
+
+    public long vymazSouboryOVelikosti(long velikost) {
+        if ((vyuziteMisto - velikost) >= 0) {
+            setVyuziteMisto(vyuziteMisto - velikost);
+            System.out.println("Nove vyuzite misto je:" + vyuziteMisto);
+        } else {
+            System.err.println("Soubory jsou prilis velke.");
+        }
+        return vyuziteMisto;
+    }
+
+
 
 }
+
