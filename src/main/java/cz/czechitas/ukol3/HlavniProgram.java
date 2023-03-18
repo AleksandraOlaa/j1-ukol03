09f9b488d0b18e375531b59f418766cea1a56a35
Aleksandra <ola.otlowska@gmail.com>
2023-03-18 12:41:04 +0100

    Opraveny kod - Hlavni program

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -16,29 +16,34 @@ public class HlavniProgram {
         Pamet myPamet = new Pamet();
         myPamet.setKapacita(14_555_000_000L);
         myPamet.setRam("pamet ram");
-        System.out.println(myPamet.getPamet());
+        System.out.println(myPamet);
 
         Dysk myDysk = new Dysk();
         myDysk.setKapacitaDysku(248_567_382_105L);
         myDysk.setVyuziteMisto(184_999_452_908L);
         myDysk.setPevnyDysk("pevny dysk");
-        System.out.println(myDysk.getDysk());
+        System.out.println(myDysk);
 
         Procesor myProcesor = new Procesor();
         myProcesor.setVyrobce("Intel");
         myProcesor.setRychlost(3_400_000_000_000L);
         myProcesor.setCpu("jeden procesor s jednym jadrem");
-        System.out.println(myProcesor.getProcesor());
+        System.out.println(myProcesor);
 
         Pocitac myPocitac;
         myPocitac = new Pocitac();
-        myPocitac.isJeZapnuty();
+        myPocitac.jeZapnuty();
         myPocitac.vypniSe();
-        myPocitac.isJeZapnuty();
+        myPocitac.jeZapnuty();
         myPocitac.zapniSe();
-        myPocitac.isJeZapnuty();
+        myPocitac.jeZapnuty();
 
-       //myDysk.vymazSouboryOVelikosti(200L);
+       myDysk.vytvorSouborOVelikosti(44_302_501_282L);
+       System.out.println(myDysk);
+        myDysk.vytvorSouborOVelikosti(340_608_704_336L);
+       myDysk.vymazSouboryOVelikosti(30_102_893_512L);
+       System.out.println(myDysk);
+       myDysk.vymazSouboryOVelikosti(200_498_532_111L);
         }
     }
 
